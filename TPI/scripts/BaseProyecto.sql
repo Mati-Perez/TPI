669973c870728493e8ec8421d367b0932bc8e10d
Mati-Perez <75102066+Mati-Perez@users.noreply.github.com>
2025-05-29 20:26:57 -0300

    Agregue las tablas socio, no socio, cuotas, actividades y pago diario a la base de datos

--- TPI/scripts/BaseProyecto.sql
+++ TPI/scripts/BaseProyecto.sql
@@ -24,3 +24,59 @@ constraint fk_usuario foreign key(RolUsu) references roles(RolUsu)
 
 insert into usuario(CodUsu,NombreUsu,PassUsu,RolUsu) values
 (26,'admin','123456',120)
+
+
+create table socio(
+NumCarnet int,
+Nombre varchar(30),
+Apellido varchar(20),
+TipoDoc varchar(20),
+Documento int,
+Direccion varchar(20),
+FechaInscripcion date,
+carnet boolean,
+
+constraint pk_socio primary key(NumCarnet)
+);
+
+create table cuota(
+	IdCuota int,
+    NumCarnet int,
+    Monto float,
+    Tipo varchar(10),
+    FechaPago date,
+    FechaVencimiento date,
+    Estado boolean,
+    constraint pk_cuota primary key(IdCuota),
+    constraint fk_cuota foreign key(NumCarnet) references socio(NumCarnet)
+);
+
+create table noSocio(
+	IdNoSocio int primary key,
+	Nombre varchar(30),
+	Apellido varchar(20),
+    TipoDoc varchar(20),
+	Documento int,
+	Direccion varchar(20)
+);
+
+create table actividad(
+	IdActividad int auto_increment,
+    NombreActividad varchar(15),
+    Horario time,
+	constraint pk_actividad primary key(IdActividad)
+);
+
+create table pagoDiario(
+	IdPagoDiario int auto_increment,
+    IdNoSocio int,
+    IdActividad int,
+    Monto float,
+    Tipo varchar(10),
+    FechaPago date,
+    Estado boolean,
+    constraint pk_pagoDiario primary key(IdPagoDiario),
+    constraint fk_pagoDiario_noSocio foreign key(IdNoSocio) references noSocio(IdNoSocio),
+    constraint fk_pagoDiario_actividad foreign key(IdActividad) references actividad(IdActividad)
+
+);
